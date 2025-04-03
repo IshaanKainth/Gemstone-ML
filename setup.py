08a1ade5f004e017667fe4d18258765a154062f4
Ishaan <ishaankaith12@gmail.com>
2025-04-04 01:44:24 +0530

    latest commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='us_visa_approval_ml',
+    version='0.0.0',
+    author='ishaankainth',
+    author_email='ishaankaith12@gmail.com',
+    description='A machine learning project for US visa approval prediction',
+    packages=find_packages(),
+    
+)
