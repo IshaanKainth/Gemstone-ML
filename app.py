60b408538de26f9ea871f33173dcd01e5b2ffbdd
Ishaan <ishaankaith12@gmail.com>
2025-06-05 21:07:54 +0530

    FINAL

--- app.py
+++ app.py
@@ -12,23 +12,28 @@ def predict_datapoint():
     if request.method == 'GET':
         return render_template('form.html')
     else:
-        data=CustomData(
-            carat=float(request.form.get('carat')),
-            cut=str(request.form.get('cut')),
-            color=str(request.form.get('color')),
-            clarity=str(request.form.get('clarity')),
-            depth=float(request.form.get('depth')),
-            table=float(request.form.get('table')),
-            x=float(request.form.get('x')),
-            y=float(request.form.get('y')),
-            z=float(request.form.get('z'))
-        )
-        final_df=data.get_data_as_df()
-        predict_pipeline=PredictionPipeline()
+        try:
+            data=CustomData(
+                carat=float(request.form.get('carat')),
+                cut=str(request.form.get('cut')),
+                color=str(request.form.get('color')),
+                clarity=str(request.form.get('clarity')),
+                depth=float(request.form.get('depth')),
+                table=float(request.form.get('table')),
+                x=float(request.form.get('x')),
+                y=float(request.form.get('y')),
+                z=float(request.form.get('z'))
+            )
+            final_df=data.get_data_as_df()
+            predict_pipeline=PredictionPipeline()
 
-        pred=predict_pipeline.prediction(final_df)
+            pred=predict_pipeline.prediction(final_df)
 
-        return render_template('submit.html',prediction=round(pred[0],2))
+            return render_template('submit.html',prediction=round(pred[0],2))
+        except Exception as e:
+        
+            print("ERROR:", e)
+            return "An error occurred: " + str(e), 500
 
 
 import os
